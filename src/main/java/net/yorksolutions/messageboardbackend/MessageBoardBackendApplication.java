6033895fb7c35463e72aea8ffecde00fb10f5646
ninjaguydan <dthompson0190@gmail.comm>
2022-07-28 19:08:57 -0500

    first commit

---
+++ src/main/java/net/yorksolutions/messageboardbackend/MessageBoardBackendApplication.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.messageboardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBoardBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MessageBoardBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/yorksolutions/messageboardbackend/MessageBoardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.messageboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBoardBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
